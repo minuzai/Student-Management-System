828a91fd5f3fca5b254a5adbe5edc797130f4d29
CreamPuffx2 <exploit017@gmail.com>
2020-10-09 00:09:07 +0900

    [DEPLY] 헤로쿠 배포

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
@@ -88,4 +89,5 @@ def addAssignment(id):
    return redirect(url_for('userList'))
 
 if __name__ == "__main__":
-   app.run()
+   port = int(os.environ.get("PORT", 5000")
+   app.run(host="0.0.0.0", port=port)
