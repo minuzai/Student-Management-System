19bc57b09cea9a4567f816fc3b5839bc644e909a
CreamPuffx2 <exploit017@gmail.com>
2020-10-09 16:59:54 +0900

    [EDIT] minor bug fix

--- app.py
+++ app.py
@@ -89,5 +89,5 @@ def addAssignment(id):
    return redirect(url_for('userList'))
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000")
+   port = int(os.environ.get("PORT", "5000"))
    app.run(host="0.0.0.0", port=port)
